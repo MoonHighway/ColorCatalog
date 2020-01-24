28a88a41b7e96426aa16f7c2eaf43239b902c105
MoonTahoe <alex@moonhighway.com>
2020-01-24 14:27:05 -0800

    extracts component file

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import { StyleSheet, View } from "react-native";
+import ColorButton from "./components/ColorButton";
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <TouchableHighlight
-        style={styles.button}
-        onPress={() => setBackgroundColor("yellow")}
-        underlayColor="orange"
-      >
-        <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor: "yellow" }]} />
-          <Text style={styles.buttonText}>yellow</Text>
-        </View>
-      </TouchableHighlight>
+      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
     </View>
   );
 }
@@ -25,28 +21,5 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
-  },
-  button: {
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255,255,255, .8)"
-  },
-  buttonText: {
-    fontSize: 30,
-    textAlign: "center"
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  sample: {
-    height: 20,
-    width: 20,
-    margin: 5,
-    borderRadius: 10,
-    backgroundColor: "white"
   }
 });
