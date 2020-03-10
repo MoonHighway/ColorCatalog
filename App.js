624ded38af7f6a401a1c874e2a068acac91705aa
MoonTahoe <alex@moonhighway.com>
2020-03-09 19:29:37 -0700

    Final Lesson

--- App.js
+++ App.js
@@ -1,33 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ActivityIndicator, Image } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  Image,
+  RefreshControl
+} from "react-native";
 import Constants from "expo-constants";
 
-const PetDetails = ({ name, photo }) => (
-  <View>
-    <Image style={styles.pic} source={{ uri: photo.full }} />
-    <Text style={styles.paragraph}>{name}</Text>
-  </View>
-);
-
 export default function App() {
+  const [loading, setLoading] = useState(false);
   const [pet, setPet] = useState();
 
   const loadPet = async () => {
+    setLoading(true);
     const res = await fetch("http://pet-library.moonhighway.com/api/randomPet");
     const data = await res.json();
+    console.log("loading image");
     await Image.prefetch(data.photo.full);
+    console.log(r);
+    console.log("image loaded");
     setPet(data);
+    setLoading(false);
   };
 
   useEffect(() => {
     loadPet();
   }, []);
 
+  if (!pet) return <ActivityIndicator size="large" />;
+
   return (
-    <View style={styles.container}>
-      {!pet && <ActivityIndicator size="large" />}
-      {pet && <PetDetails {...pet} />}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={loadPet} />
+        }
+      >
+        <Image
+          style={styles.pic}
+          source={{ uri: pet.photo.full, cache: "force-cache" }}
+        />
+        <Text style={styles.paragraph}>
+          {pet.name} - {pet.category}
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
