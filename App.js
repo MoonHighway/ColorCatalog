696f275c0822327000f11732e7101682d82f3360
MoonTahoe <alex@moonhighway.com>
2020-03-09 18:15:07 -0700

    Step 1 loading pets

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
-import React from "react";
-import ColorList from "./components/ColorList";
-import ColorDetails from "./components/ColorDetails";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ActivityIndicator, Image } from "react-native";
+import Constants from "expo-constants";
 
-const { Navigator, Screen } = createStackNavigator();
+const PetDetails = ({ name, photo }) => (
+  <View>
+    <Image style={styles.pic} source={{ uri: photo.full }} />
+    <Text style={styles.paragraph}>{name}</Text>
+  </View>
+);
 
 export default function App() {
+  const [pet, setPet] = useState();
+
+  const loadPet = async () => {
+    const res = await fetch("http://pet-library.moonhighway.com/api/randomPet");
+    const data = await res.json();
+    await Image.prefetch(data.photo.full);
+    setPet(data);
+  };
+
+  useEffect(() => {
+    loadPet();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen
-          name="Home"
-          options={{ title: "Color List" }}
-          component={ColorList}
-        />
-        <Screen name="Details" component={ColorDetails} />
-      </Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {!pet && <ActivityIndicator size="large" />}
+      {pet && <PetDetails {...pet} />}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  pic: {
+    width: 500,
+    height: 500
+  }
+});
