cdcfa29f247af0cba0821345403b9621cf7ffa31
MoonTahoe <alex@moonhighway.com>
2020-03-20 19:33:33 -0700

    iOS Production Progress
    * Incorporates Drawer Navigator
    * Creates Context For Colors
    * Stubs Settings Page
    * Prevents Duplicate colors from being added

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React from "react";
 import ColorList from "./components/ColorList";
 import ColorDetails from "./components/ColorDetails";
+import Settings from "./components/Settings";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { ColorProvider } from "./hooks";
 
 const { Navigator, Screen } = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainScreens = () => (
+  <Navigator>
+    <Screen
+      name="Home"
+      options={{ title: "Color List" }}
+      component={ColorList}
+    />
+    <Screen name="Details" component={ColorDetails} />
+  </Navigator>
+);
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen
-          name="Home"
-          options={{ title: "Color List" }}
-          component={ColorList}
-        />
-        <Screen name="Details" component={ColorDetails} />
-      </Navigator>
-    </NavigationContainer>
+    <ColorProvider>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Main" component={MainScreens} />
+          <Drawer.Screen name="Settings" component={Settings} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </ColorProvider>
   );
 }
--- hooks.js
+++ hooks.js
@@ -1,8 +1,31 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext, useContext } from "react";
 import { generate } from "shortid";
 import { AsyncStorage } from "react-native";
+import Color from "color";
 
-export const useColors = () => {
+const colorPredicate = color => c => c.color === color;
+const colorNotFound = (color, colors) => !colors.some(colorPredicate);
+const colorExists = (color, arr = []) => {
+  if (arr.some(colorPredicate(color)))
+    throw new Error(
+      `Color ${color} already exists in the list at index ${arr.findIndex(
+        c => c.color === color
+      )}`
+    );
+};
+const invalidColor = color => {
+  try {
+    new Color(color);
+    return false;
+  } catch {
+    return true;
+  }
+};
+
+const ColorContext = createContext([]);
+export const useColors = () => useContext(ColorContext);
+
+export const ColorProvider = ({ children }) => {
   const [colors, setColors] = useState([]);
 
   const loadColors = async () => {
@@ -13,20 +36,36 @@ export const useColors = () => {
     }
   };
 
-  // Load Colors
   useEffect(() => {
     if (colors.length) return;
     loadColors();
   }, []);
 
-  // Save Colors
   useEffect(() => {
     AsyncStorage.setItem("@ColorListStore:Colors", JSON.stringify(colors));
   }, [colors]);
 
   const addColor = color => {
+    if (invalidColor(color)) return;
+    if (colorExists(color, colors)) return;
     const newColor = { id: generate(), color };
     setColors([newColor, ...colors]);
   };
-  return { colors, addColor };
+
+  const removeColor = color => {
+    if (colorNotFound(color)) return;
+    const newColors = colors.filter(colorPredicate);
+    setColors(newColors);
+  };
+
+  const clearColors = () => {
+    AsyncStorage.removeItem("@ColorListStore:Colors");
+    setColors([]);
+  };
+
+  return (
+    <ColorContext.Provider value={{ colors, addColor, removeColor }}>
+      {children}
+    </ColorContext.Provider>
+  );
 };
